cf5c3103724f5a53bcff12ca5d6473b0bdddb8e1
Immijimmi <immijimmi1@gmail.com>
2021-02-12 21:45:04 +0000

    Updated fixtures

--- test/conftest.py
+++ test/conftest.py
@@ -1,21 +1,48 @@
 import pytest
+from tkinter import Tk, Button
 
 from tkcomponents import Component
 
 
 @pytest.fixture
-def blank_parent_cls():
-    class Blank(Component):
+def window():
+    return Tk()
+
+
+@pytest.fixture
+def nested_button_cls():
+    class ButtonWrapper(Component):
+        def __init__(self, container, get_data=None, on_change=lambda: None,
+                     update_interval=None, styles=None):
+            super().__init__(container, get_data=get_data, on_change=on_change,
+                             update_interval=update_interval, styles=styles)
+
+            styles = {} if not styles else styles
+            self.styles["button"] = styles.get("button", {})
+
         def _render(self):
-            pass
+            self.children["button"] = None
+
+            button = Button(self._frame, **self.styles["button"])
+            self.children["button"] = button
+
+            button.pack()
 
     class Parent(Component):
+        def __init__(self, container, get_data=None, on_change=lambda: None,
+                     update_interval=None, styles=None):
+            super().__init__(container, get_data=get_data, on_change=on_change,
+                             update_interval=update_interval, styles=styles)
+
+            styles = {} if not styles else styles
+            self.styles["button_wrapper"] = styles.get("button_wrapper", {})
+
         def _render(self):
-            self.children["blank"] = None
+            self.children["button_wrapper"] = None
 
-            blank = Blank(self._frame)
-            self.children["blank"] = blank
+            button_wrapper = ButtonWrapper(self._frame, styles=self.styles["button_wrapper"])
+            self.children["button_wrapper"] = button_wrapper
 
-            blank.render().pack()
+            button_wrapper.render().pack()
 
     return Parent
